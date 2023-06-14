db53bbf39352cea5779d51cdfb4cd01c7c045a05
bixoftware <136520091+bixoftware@users.noreply.github.com>
2023-06-14 12:43:19 +0000

    Updates

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+const qrcode = require('qrcode-terminal');
+const { Client } = require('whatsapp-web.js');
 
+const client = new Client();
+
+client.initialize();
+
+client.on('qr', (qr) => {
+  qrcode.generate(qr, { small: true });
+});
+
+client.on('ready', () => {
+  console.log('Client is ready!');
+});
+
+client.on('message', (message) => {
+  if (message.body === 'hello') {
+    message.reply('Thank you for messaging Toonitt Media Bot 😊, ask me anything about Toonitt Media');
+  }
+});
